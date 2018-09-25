d2854d9470f6447fca4084a01e0b2d4d2040b469
Vadoniy <kurbatov_1989@inbox.ru>
2018-09-25 19:49:54 +0300

    Lab_4. build.gradle

---
+++ tracker-core/src/main/java/jdev/application/Application.java
@@ -0,0 +1,7 @@
+package jdev.application;
+
+/**
+ * Created by Vadoniy on 22.09.2018.
+ */
+public class Application {
+}
---
+++ tracker-core/src/main/java/jdev/application/Main.java
@@ -0,0 +1,13 @@
+package jdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"context", "services"})
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
