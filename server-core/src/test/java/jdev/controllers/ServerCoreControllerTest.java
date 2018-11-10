a657ca28aa0e459a74e4737ee3ec01e1ba41ced1
Vadoniy <kurbatov_1989@inbox.ru>
2018-11-11 00:01:49 +0300

    Added JUnit tests for PointDTO to all modules

--- server-core/src/test/java/jdev/controllers/ServerCoreControllerTest.java
+++ server-core/src/test/java/jdev/controllers/ServerCoreControllerTest.java
@@ -1,8 +1,36 @@
-import static org.junit.Assert.*;
+package jdev.controllers;
 
-/**
- * Created by Vadoniy on 10.11.2018.
- */
+import dto.PointDTO;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ServerCoreControllerTest {
 
+private static String pointDataJsonTest = "{\"lat\":20.5,\"lon\":12.5,\"autoId\":\"R934SK777\",\"time\":1234567891}";
+
+    @InjectMocks
+    ServerCoreController mockedController;
+
+    private PointDTO pointDTO;
+
+    @Before
+    public void setPointDTO(){
+        pointDTO = new PointDTO();
+        pointDTO.setAutoId("R934SK777");
+        pointDTO.setLon(12.5);
+        pointDTO.setLat(20.5);
+        pointDTO.setTime(1234567891);
+    }
+
+    @Test
+    public void createPoint() throws Exception {
+        PointDTO testPointDTO = mockedController.createPoint(pointDTO);
+        assertEquals(testPointDTO.toJson(), pointDataJsonTest);
+    }
 }
--- tracker-ui/src/test/java/PointDTOTestTU.java
+++ tracker-ui/src/test/java/PointDTOTestTU.java
@@ -1,7 +1,33 @@
-package PACKAGE_NAME;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dto.PointDTO;
+import org.junit.Test;
 
-/**
- * Created by Vadoniy on 18.07.2018.
- */
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class PointDTOTestTU {
+    private String expected = "{\"lat\":17.0,\"lon\":11.0,\"autoId\":\"P934CK777\",\"time\":65478392}";
+    private String autoId = "P934CK777";
+
+    @Test
+    public void toJson() throws JsonProcessingException {
+        PointDTO point = new PointDTO(17, 11, "Р934СК", System.currentTimeMillis());
+        assertTrue(point.toJson().contains("\"autoId\":\"Р934СК\""));
+    }
+
+    @Test
+    public void decodeDto() throws IOException {
+        PointDTO point = new ObjectMapper().readValue(expected, PointDTO.class);
+        assertEquals(point.getAutoId(), autoId);
+    }
+
+    @Test
+    public void checkAutoId() throws IOException {
+        PointDTO point = new ObjectMapper().readValue(expected, PointDTO.class);
+        assertTrue(point.toJson().contains("autoId"));
+        System.out.println(point.toJson());
+    }
 }
